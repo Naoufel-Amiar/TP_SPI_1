f485194dcb9fb847b8c21f3566ffa8deb53c0e83
Naoufel Amiar <naoufel.amiar@saintmichelannecy.fr>
2025-10-09 12:21:53 +0200

    Initial commit: projet Code::Blocks

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    //add 2 number
+    int a,b,c;
+    printf("enter the first number\n");
+    scanf("%d",&a);
+    printf("enter the second number\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The Number is %d",c);
+
+}
